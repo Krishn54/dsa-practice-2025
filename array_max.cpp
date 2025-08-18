1d4b71e40c5a4841606d87885d916a5d697380eb
Krishn54 <krishrana548@gmail.com>
2025-08-18 20:00:49 +0530

    max element with user input

---
+++ array_max.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << *max_element(arr.begin(), arr.end());
+    return 0;
+
+}
