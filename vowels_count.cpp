1caa14d86aca5e07b973d1aad5586fefa85ead98
Krishn54 <krishrana548@gmail.com>
2025-08-21 20:12:19 +0530

    vowels and consononants count

--- vowels_count.cpp
+++ vowels_count.cpp
@@ -6,12 +6,17 @@ int main(){
     string str;
     cin>>str;
     int vowelsCount = 0;
+    int consonants=0;
     for(char c : str){
         c = tolower(c);
         if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
             vowelsCount++;
         }
+        else{
+            consonants++;
+        }
     }
     cout<<"Number of vowels: "<<vowelsCount<<endl;
+    cout<<"Number of consonants: "<<consonants;
     return 0;
 }
