4d387a52d7e8ce33214af22203212de08cb4356e
Krishn54 <krishrana548@gmail.com>
2025-08-23 14:11:13 +0530

    prime no check

---
+++ check_prime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool checkPrime(int n){
+    if(n<=1) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cin >>n;
+    if(checkPrime(n)) cout << n << " is prime";
+    else cout << n << " is not prime";
+    return 0;
+}
